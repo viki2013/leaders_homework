bc7596058f6dd068396b6fefe624e65f8ef9694d
viki2013 <viktoriasikharulidze@gmail.com>
2024-12-24 11:24:01 -0800

    Update 1.py

---
+++ first_homework/1.py
@@ -0,0 +1,29 @@
+num1 = "hello world"
+print(num1)
+
+user = "hello"
+print(user)
+
+num1 = "10"
+num2 = "28"
+num3 = num1 + num2
+print(num3)
+
+number 1 = int(input(10))
+number 2 = int(input(11))
+print(number 1)
+print(number 2)
+
+age = 11
+age 2 = age * 2
+print(age2)
+
+favourite_movies = "grinch , alone at home and so all"
+print(favourite_movies)
+
+for i in range(1,11):
+    print(i)
+
+numbers = "1,20,634,121"
+sum_of_numbers = 1 + 20 + 634 + 121
+print(sum_of_numbers)
